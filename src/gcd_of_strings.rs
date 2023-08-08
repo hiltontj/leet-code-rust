dd8c9848fa7d9e776c5edd35c9b18ebbf0080baf
Trevor Hilton <trevor.hilton@gmail.com>
2023-08-08 12:18:55 -0400

    comment gcd of strings

--- src/gcd_of_strings.rs
+++ src/gcd_of_strings.rs
@@ -3,17 +3,13 @@
 /// For two strings `s1` and `s2`, find the largest string that
 /// divides them both.
 pub fn gcd_of_strings(s1: &str, s2: &str) -> String {
-    // TODO - make loop index non-linear
-    let end = s1.len().min(s2.len());
-    println!("end: {end}");
-    for i in DivisorRange::new(end).rev() {
+    let size = s1.len().min(s2.len());
+    for i in divisors_of(size).rev() {
         let (left, _) = s1.split_at(i);
-        println!("left: {left}");
         if left.divides(s2) && left.divides(s1) {
             return left.to_string();
         }
         let (left, _) = s2.split_at(i);
-        println!("left: {left}");
         if left.divides(s1) && left.divides(s2) {
             return left.to_string();
         }
@@ -21,13 +17,42 @@ pub fn gcd_of_strings(s1: &str, s2: &str) -> String {
     Default::default()
 }
 
+/// Helper function for creating a new [`DivisorRange`]
+fn divisors_of(number: usize) -> DivisorRange {
+    DivisorRange::new(number)
+}
+
+/// Generate a range of divisors
+///
+/// Given a number, this will allow one to iterate over that number's even divisors.
+///
+/// # Usage
+/// ```ignore
+/// for i in divisors_of(12) {
+///     print!("{i} ");
+/// }
+/// // prints: 1 2 3 4 6 12
+/// ```
+///
+/// [`DivisorRange`] implements [`DoubleEndedIterator`] so that you can iterate over
+/// divisors in reverse:
+/// ```ignore
+/// let number = 6;
+/// for i in divisors_of(6).rev() {
+///     print!("{i} ");
+/// }
+/// // prints: 6 3 2 1
+/// ```
 #[derive(Debug)]
 struct DivisorRange {
+    /// The number, the divisors of which will be iterated over
     number: usize,
+    /// Used for tracking iteration location
     current: usize,
 }
 
 impl DivisorRange {
+    /// Create a new `DivisorRange` for the given `number`
     fn new(number: usize) -> Self {
         Self { number, current: 1 }
     }
@@ -50,7 +75,6 @@ impl Iterator for DivisorRange {
 
 impl DoubleEndedIterator for DivisorRange {
     fn next_back(&mut self) -> Option<Self::Item> {
-        println!("next_back: {self:?}");
         let v = self.number / self.current;
         while self.current <= self.number {
             self.current += 1;
@@ -62,6 +86,7 @@ impl DoubleEndedIterator for DivisorRange {
     }
 }
 
+/// A trait to define if one things _divides_ another
 trait Divisor<Rhs = Self> {
     fn divides(self, other: Rhs) -> bool;
 }
@@ -74,13 +99,52 @@ impl<'a> Divisor for &'a str {
         if other.len() % self.len() != 0 {
             return false;
         }
-        let mut check = String::new();
-        for _ in 0..(other.len() / self.len()) {
-            check.push_str(self);
+        for Window(start, end) in SlidingWindow::new(0, other.len(), self.len()) {
+            if &other[start..end] != self {
+                return false;
+            }
         }
-        check.as_str() == other
+        true
     }
 }
+
+/// A sliding window iterator
+///
+/// Used to generate indicies for a sliding window. A window, of `size`, starting from the index `start`,
+/// is slid along a range of indices, ending at `end`.
+#[derive(Debug)]
+struct SlidingWindow {
+    /// The starting index of the window
+    start: usize,
+    /// The ending index of the total range being considered
+    end: usize,
+    /// The size of the window
+    size: usize,
+}
+
+impl SlidingWindow {
+    /// Create a new [`SlidingWindow`]
+    fn new(start: usize, end: usize, size: usize) -> Self {
+        Self { start, end, size }
+    }
+}
+
+impl Iterator for SlidingWindow {
+    type Item = Window;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.start + self.size > self.end {
+            None
+        } else {
+            let window = Window(self.start, self.start + self.size);
+            self.start += self.size;
+            Some(window)
+        }
+    }
+}
+
+/// The iterator item for [`SlidingWindow`]
+struct Window(usize, usize);
 
 #[cfg(test)]
 mod tests {
